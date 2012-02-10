e655c05f62969bf88730f3d4904403063c280ed8
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-02-10 09:51:51 -0600

    Fixed add field issue

--- javascript/workflow.js
+++ javascript/workflow.js
@@ -1,15 +1,11 @@
-var $field_clone = 0
 $(document).ready(function() {
-  $field_clone = $('.cloneable_module_field').clone();
-  $field_clone = $field_clone.removeAttr("class").attr("class", "module_field");
   $('#add_field_button').click(function(){
-    $('div.module_field').after($field_clone);
-    $field_clone.find('input[name*="delete"]').click(function(){
+    $('div.module_field:last').after(field_clone);
+    field_clone.find('input[name*="delete"]').click(function(){
       if ($('.module_field').size() != 1)
 	$(this).closest('div.module_field').remove();
     });
   });
-
   $('input[name*="delete"]').click(function(){
     if ($('.module_field').size() != 1)
       $(this).closest('div.module_field').remove();
@@ -30,3 +26,22 @@ $(document).ready(function() {
     }
   });
 });
+
+var field_clone = '<div class="module_field">' +
+    '<div class="field_name">Field Name: <input type="text" name="field_name" /></div>' +
+    '<div class="field_type">Type: ' +
+      '<select>' +
+	'<option value="integer">Integer</option>' +
+	'<option value="string">String</option>' +
+	'<option value="datetime">Datetime</option>' +
+	'<option value="date">Date</option>' +
+      '</select>' +
+    '</div>' +
+    '<div class="field_parent_module">Parent Module (if one): <input type="text" name="parent_module"/></div>' +
+    '<div class="acceptable_value_container">' +
+      '<div class="field_acceptable_values">Acceptable Values<input type="text" name="acceptable_value_text_field"/><button id="add_button" type="button">Add Value</button></div>' +
+      '<div class="acceptable_value_list"></div>' +
+    '</div>' +
+    '<div class="delete_field_button"><input type="image" name="delete_field" src="images/delete_circle.jpg"/></div>' +
+   ' <hr>' +
+  '</div>'
